21f5cb52c71736450b0b13d6d22824afbfa326ca
AoiSejima <sejia949@newschool.edu>
2021-10-12 17:44:01 -0400

    Added slides, got rid of round frame around images

--- script.js
+++ script.js
@@ -11,103 +11,43 @@ aboutContainer.addEventListener("mouseover", function(){
     aboutSection.style.display = "block";
 });
 
+// window.addEventListener("load", function() {
+    
+//     let carouselSlides = document.querySelectorAll('.carousel-slide');
+//     let currentSlide = 0;
+//     let total = carouselSlides.length;
 
-// click each image to display each description container
+//     let nextBtn = document.querySelector('.next-btn');
+//     let prevBtn = document.querySelector('.prev-btn');
 
-// display cross out button after images are clicked
-// hide it after cross out button is clicked
-let crossButton = document.querySelector("button");
-let backgroundFilter = document.getElementById("background-filter");
+//     function showNextSlide() {
+//         hideAllSlides();
+//         if(currentSlide === total - 1){
+//             currentSlide = 0;
+//         }
+//         else {
+//             currentSlide++;
+//         }
+//         carouselSlides[currentSlide].style.opacity = 1;
+//         console.log(currentSlide)
+//     }
+//     function showPrevSlide() {
+//         hideAllSlides();
+//         if(currentSlide === 0){
+//             currentSlide = total - 1;
+//         }
+//         else {
+//             currentSlide--;
+//         }
+//         carouselSlides[currentSlide].style.opacity = 1;
+//         console.log(currentSlide)
+//     }
+//     function hideAllSlides() {
+//         carouselSlides.forEach(function(slide){
+//             slide.style.opacity = 0;
+//         });
+//     }
 
-// WEBSITE
-let techCollectionImage = document.getElementById("techCollection-image");
-let techCollectionContainer = document.getElementById("techCollection-container");
-techCollectionImage.addEventListener("click", function(){
-    techCollectionContainer.style.display = "block";
-    crossButton.style.display = "block";
-    backgroundFilter.style.display = "block";
-});
-
-let inPraiseOfShadowImage = document.getElementById("inPraiseOfShadow-image");
-let inPraiseOfShadowContainer = document.getElementById("inPraiseOfShadow-container");
-inPraiseOfShadowImage.addEventListener("click", function(){
-    inPraiseOfShadowContainer.style.display = "block";
-    crossButton.style.display = "block";
-    backgroundFilter.style.display = "block";
-});
-
-let urbanNatureImage = document.getElementById("urbanNature-image");
-let urbanNatureContainer = document.getElementById("urbanNature-container");
-urbanNatureImage.addEventListener("click", function(){
-    urbanNatureContainer.style.display = "block";
-    crossButton.style.display = "block";
-    backgroundFilter.style.display = "block";
-});
-
-let treasureHuntImage = document.getElementById("treasureHunt-image");
-let treasureHuntContainer = document.getElementById("treasureHunt-container");
-treasureHuntImage.addEventListener("click", function(){
-    treasureHuntContainer.style.display = "block";
-    crossButton.style.display = "block";
-    backgroundFilter.style.display = "block";
-});
-
-// TYPOGRAPHY
-let wikiBookMoonImage = document.getElementById("wikiBookMoon-image");
-let wikibookMoonContainer = document.getElementById("wikibookMoon-container");
-wikiBookMoonImage.addEventListener("click", function(){
-    wikibookMoonContainer.style.display = "block";
-    crossButton.style.display = "block";
-    backgroundFilter.style.display = "block";
-});
-
-let interviewTrustImage = document.getElementById("interviewTrust-image");
-let interviewTrustContainer = document.getElementById("interviewTrust-container");
-interviewTrustImage.addEventListener("click", function(){
-    interviewTrustContainer.style.display = "block";
-    crossButton.style.display = "block";
-    backgroundFilter.style.display = "block";
-});
-
-let gameCenterImage = document.getElementById("gameCenter-image");
-let gameCenterContainer = document.getElementById("gameCenter-container");
-gameCenterImage.addEventListener("click", function(){
-    gameCenterContainer.style.display = "block";
-    crossButton.style.display = "block";
-    backgroundFilter.style.display = "block";
-});
-
-let croppedLettersImage = document.getElementById("croppedLetters-image");
-let croppedLettersContainer = document.getElementById("croppedLetters-container");
-croppedLettersImage.addEventListener("click", function(){
-    croppedLettersContainer.style.display = "block";
-    crossButton.style.display = "block";
-    backgroundFilter.style.display = "block";
-});
-
-// ANIMATION
-let by2072Image = document.getElementById("by2072-image");
-let by2072Container = document.getElementById("by2072-container");
-by2072Image.addEventListener("click", function(){
-    by2072Container.style.display = "block";
-    crossButton.style.display = "block";
-    backgroundFilter.style.display = "block";
-});
-
-// cross button
-crossButton.addEventListener("click", function(){
-    crossButton.style.display = "none";
-    backgroundFilter.style.display = "none";
-    // WEBSITE
-    techCollectionContainer.style.display = "none";
-    inPraiseOfShadowContainer.style.display = "none";
-    urbanNatureContainer.style.display = "none";
-    treasureHuntContainer.style.display = "none";
-    // TYPOGRAPHY
-    wikibookMoonContainer.style.display = "none";
-    interviewTrustContainer.style.display = "none"; 
-    gameCenterContainer.style.display = "none";
-    croppedLettersContainer.style.display = "none";
-    // ANIMATION
-    by2072Container.style.display = "none"; 
-});
+//     nextBtn.addEventListener('click',showNextSlide);
+//     prevBtn.addEventListener('click',showPrevSlide);
+// });
