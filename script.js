68455564ab7c998522a0cb7de367edef3365f02d
AoiSejima <sejia949@newschool.edu>
2021-10-12 19:41:37 -0400

    Edited the details

--- script.js
+++ script.js
@@ -1,53 +1,11 @@
 // console.log("Hello");
 
-// hover aside for about insert
-let aboutContainer = document.querySelector("aside");
-let aboutSection = document.getElementById("about");
+let counter = 1;
 
-aboutContainer.addEventListener("mouseout", function(){
-    aboutSection.style.display = "none";
-});
-aboutContainer.addEventListener("mouseover", function(){
-    aboutSection.style.display = "block";
-});
-
-// window.addEventListener("load", function() {
-    
-//     let carouselSlides = document.querySelectorAll('.carousel-slide');
-//     let currentSlide = 0;
-//     let total = carouselSlides.length;
-
-//     let nextBtn = document.querySelector('.next-btn');
-//     let prevBtn = document.querySelector('.prev-btn');
-
-//     function showNextSlide() {
-//         hideAllSlides();
-//         if(currentSlide === total - 1){
-//             currentSlide = 0;
-//         }
-//         else {
-//             currentSlide++;
-//         }
-//         carouselSlides[currentSlide].style.opacity = 1;
-//         console.log(currentSlide)
-//     }
-//     function showPrevSlide() {
-//         hideAllSlides();
-//         if(currentSlide === 0){
-//             currentSlide = total - 1;
-//         }
-//         else {
-//             currentSlide--;
-//         }
-//         carouselSlides[currentSlide].style.opacity = 1;
-//         console.log(currentSlide)
-//     }
-//     function hideAllSlides() {
-//         carouselSlides.forEach(function(slide){
-//             slide.style.opacity = 0;
-//         });
-//     }
-
-//     nextBtn.addEventListener('click',showNextSlide);
-//     prevBtn.addEventListener('click',showPrevSlide);
-// });
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 5){
+        counter = 1;
+    }
+}, 5000);
