a88f4de340c1a832a4dc546189c433876b3a637c
AoiSejima <sejia949@newschool.edu>
2021-10-13 14:52:56 -0400

    Updated

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // console.log("Hello");
 
-let counter = 1;
+// let counter = 1;
 
-setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if(counter > 5){
-        counter = 1;
-    }
-}, 5000);
+// setInterval(function(){
+//     document.getElementById('radio' + counter).checked = true;
+//     counter++;
+//     if(counter > 5){
+//         counter = 1;
+//     }
+// }, 5000);
